1db4e49fcfb7f747153e1717dd3b47afcf0d3eee
Pepe Ortiz <pepe.ortiz@gfrmedia.com>
2013-04-30 11:39:43 -0400

    committing latest changes

---
+++ javascripts/photogalleries.js
@@ -0,0 +1,127 @@
+	// Load dialog on click
+	$('.galPop01').click(function (e) {
+
+		// Display an external page using an iframe
+		var src = "gal01.html";
+		$.modal('<iframe src="' + src + '" height="730" width="500" frameborder=0 marginheight=0 marginwidth=0 style="border:0">', {
+			closeHTML:"",
+			containerCss:{
+				backgroundColor:"#fff", 
+				borderColor:"#fff", 
+				height:745,
+				padding:0, 
+				width:525
+			},
+			overlayClose:true
+		});
+		return false;
+	});	
+	
+	$('.galPop02').click(function (e) {
+
+		// Display an external page using an iframe
+		var src = "gal02.html";
+		$.modal('<iframe src="' + src + '" height="755" width="1080" frameborder=0 marginheight=0 marginwidth=0 style="border:0">', {
+			closeHTML:"",
+			containerCss:{
+				backgroundColor:"#fff", 
+				borderColor:"#fff", 
+				height:770,
+				padding:0, 
+				width:1105
+			},
+			overlayClose:true
+		});
+		return false;
+	});
+	
+	
+	$('.galPop04').click(function (e) {
+
+		// Display an external page using an iframe
+		var src = "gal04.html";
+		$.modal('<iframe src="' + src + '" height="755" width="1080" frameborder=0 marginheight=0 marginwidth=0 style="border:0">', {
+			closeHTML:"",
+			containerCss:{
+				backgroundColor:"#fff", 
+				borderColor:"#fff", 
+				height:770,
+				padding:0, 
+				width:1105
+			},
+			overlayClose:true
+		});
+		return false;
+	});	
+	
+	$('.galPop08').click(function (e) {
+
+		// Display an external page using an iframe
+		var src = "gal08.html";
+		$.modal('<iframe src="' + src + '" height="630" width="800" frameborder=0 marginheight=0 marginwidth=0 style="border:0">', {
+			closeHTML:"",
+			containerCss:{
+				backgroundColor:"#fff", 
+				borderColor:"#fff", 
+				height:650,
+				padding:0, 
+				width:825
+			},
+			overlayClose:true
+		});
+		return false;
+	});
+	
+	$('.galPop13').click(function (e) {
+
+		// Display an external page using an iframe
+		var src = "gal13.html";
+		$.modal('<iframe src="' + src + '" height="630" width="800" frameborder=0 marginheight=0 marginwidth=0 style="border:0">', {
+			closeHTML:"",
+			containerCss:{
+				backgroundColor:"#fff", 
+				borderColor:"#fff", 
+				height:650,
+				padding:0, 
+				width:825
+			},
+			overlayClose:true
+		});
+		return false;
+	});
+
+	$('.galPop15').click(function (e) {
+
+		// Display an external page using an iframe
+		var src = "gal15.html";
+		$.modal('<iframe src="' + src + '" height="755" width="1080" frameborder=0 marginheight=0 marginwidth=0 style="border:0">', {
+			closeHTML:"",
+			containerCss:{
+				backgroundColor:"#fff", 
+				borderColor:"#fff", 
+				height:770,
+				padding:0, 
+				width:1105
+			},
+			overlayClose:true
+		});
+		return false;
+	});
+	
+	$('.galPop17').click(function (e) {
+
+		// Display an external page using an iframe
+		var src = "gal17.html";
+		$.modal('<iframe src="' + src + '" height="755" width="1080" frameborder=0 marginheight=0 marginwidth=0 style="border:0">', {
+			closeHTML:"",
+			containerCss:{
+				backgroundColor:"#fff", 
+				borderColor:"#fff", 
+				height:770,
+				padding:0, 
+				width:1105
+			},
+			overlayClose:true
+		});
+		return false;
+	});
